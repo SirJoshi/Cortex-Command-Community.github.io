e6ce42e2197d79e7f82a032c2dec1a4bfd4cbcf0
SirJoshi <45546025+SirJoshi@users.noreply.github.com>
2020-11-18 14:18:53 +0000

    Deploying to gh-pages from  @ af8e01afff6f5877fb217d7c49fe0d92de3f62ff 🚀

---
+++ precache-manifest.d9c181369c07a0393d1b033cecf2295b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3baac5c51016dd44427934f0ec30a67a",
+    "url": "/Cortex-Command-Community.github.io/index.html"
+  },
+  {
+    "revision": "a35d0bc960ae45b14d07",
+    "url": "/Cortex-Command-Community.github.io/static/css/main.f2957721.chunk.css"
+  },
+  {
+    "revision": "11a5cf3b04344ded56e8",
+    "url": "/Cortex-Command-Community.github.io/static/js/2.9c694d9e.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/Cortex-Command-Community.github.io/static/js/2.9c694d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35d0bc960ae45b14d07",
+    "url": "/Cortex-Command-Community.github.io/static/js/main.52a099f7.chunk.js"
+  },
+  {
+    "revision": "e78142dbbf37a7d1395a",
+    "url": "/Cortex-Command-Community.github.io/static/js/runtime-main.970038e3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb4c6f020ef03480eb7f67ab63500257.js"
+  "/Cortex-Command-Community.github.io/precache-manifest.d9c181369c07a0393d1b033cecf2295b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Cortex-Command-Community.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
